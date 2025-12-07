d700529d2cf135d97a7d13092fd761ce82778c69
AkilanChithra <chithra.sathish.akilan@gmail.com>
2025-12-07 16:07:44 -0600

    added onestartchecker and much more

--- devlog.md
+++ devlog.md
@@ -17,3 +17,19 @@ LOG ENTRY 3 12/07/2025 1:46 AM:
 Now I have implemeted cell_at/4 to look up a cell in maze by 0 based row and column indices using nth0/3
 
 I tested it on the sample_maze/1, to make sure that 1,1 gives s, and 1,2 gives e  and so on
+
+
+LOG ENTRY 4 12/07/2025 3:51 PM:
+
+Woke up and now have written a lot more code
+
+
+first I hadded the find_start to find the coordinates of the start cell
+
+then I also did the hascell to check whether the symbil liek s or e is anyhwere on the maze
+I also created the basics of the validation of the maze,that uses maze_dimesions and has-cell/2 to make sure that the maze has at least one row and oclumn
+
+There is also the exactly_one_start ufnction to make sure that there is only once start cell and not many s positions on the map
+
+I also tested everythign to make sur that it all works on the sample_maze
+
--- project2.pl
+++ project2.pl
@@ -3,8 +3,6 @@ find_exit(_Maze, _Actions) :-
     fail.
 
 
-% maze_dimensions(+Maze, -NumRows, -NumCols)
-% Succeeds if Maze has NumRows rows and each row has NumCols columns.
 % For now, we assume the maze is rectangular if it loads correctly.
 maze_dimensions(Maze, NumRows, NumCols) :-
     length(Maze, NumRows),
@@ -18,12 +16,41 @@ sample_maze([
     [w, w, w]
 ]).
 
-% cell_at(+Maze, +RowIdx, +ColIdx, -Cell)
-% Using 0-based indices:
-%   RowIdx = 0 is the first row
-%   ColIdx = 0 is the first column
 % Succeeds with the Cell at that position, or fails if the indices are out of bounds.
 cell_at(Maze, RowIdx, ColIdx, Cell) :-
     nth0(RowIdx, Maze, Row),
     nth0(ColIdx, Row, Cell).
 
+find_start(Maze, RowIdx, ColIdx) :-
+    nth0(RowIdx, Maze, Row),
+    nth0(ColIdx, Row, s).
+
+
+% True if the given Cell (e.g., s or e) appears anywhere in the maze.
+has_cell(Maze, Cell) :-
+    member(Row, Maze),
+    member(Cell, Row).
+
+
+
+% valid_maze_basic(+Maze)
+% Basic sanity checks:
+%   - Maze has at least one row and one column (via maze_dimensions/3).
+%   - Maze contains at least one 's' (start).
+%   - Maze contains at least one 'e' (exit).
+valid_maze_basic(Maze) :-
+    maze_dimensions(Maze, _NumRows, _NumCols),
+    has_cell(Maze, s),
+    has_cell(Maze, e).
+
+
+% exactly_one_start(+Maze)
+% True if the maze has one 's' cell and no other distinct 's' cells.
+exactly_one_start(Maze) :-
+    % Find one start position
+    find_start(Maze, Row, Col),
+    % Assert there is no *different* second start
+    \+ (
+        find_start(Maze, Row2, Col2),
+        (Row2 \= Row ; Col2 \= Col)
+    ).
