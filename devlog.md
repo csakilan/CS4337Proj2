6d8e5963021c055739ba782c0fa48a2d7d36a0b4
AkilanChithra <chithra.sathish.akilan@gmail.com>
2025-12-07 01:52:54 -0600

    added cell_at/4 helper

--- devlog.md
+++ devlog.md
@@ -10,3 +10,10 @@ LOG ENTRY 2 12/07/2025 01:08 AM:
 I have imported all the helper files and created the base file project2.pl
 
 I have also installed swipl to run terminal commands with the open source SWI prolog CLI
+
+
+LOG ENTRY 3 12/07/2025 1:46 AM:
+
+Now I have implemeted cell_at/4 to look up a cell in maze by 0 based row and column indices using nth0/3
+
+I tested it on the sample_maze/1, to make sure that 1,1 gives s, and 1,2 gives e  and so on
--- project2.pl
+++ project2.pl
@@ -1,3 +1,29 @@
 find_exit(_Maze, _Actions) :-
     writeln('find_exit/2 stub called'),
     fail.
+
+
+% maze_dimensions(+Maze, -NumRows, -NumCols)
+% Succeeds if Maze has NumRows rows and each row has NumCols columns.
+% For now, we assume the maze is rectangular if it loads correctly.
+maze_dimensions(Maze, NumRows, NumCols) :-
+    length(Maze, NumRows),
+    Maze = [FirstRow | _],
+    length(FirstRow, NumCols).
+
+% A tiny sample maze for testing (not part of final API; just for you).
+sample_maze([
+    [w, w, w],
+    [w, s, e],
+    [w, w, w]
+]).
+
+% cell_at(+Maze, +RowIdx, +ColIdx, -Cell)
+% Using 0-based indices:
+%   RowIdx = 0 is the first row
+%   ColIdx = 0 is the first column
+% Succeeds with the Cell at that position, or fails if the indices are out of bounds.
+cell_at(Maze, RowIdx, ColIdx, Cell) :-
+    nth0(RowIdx, Maze, Row),
+    nth0(ColIdx, Row, Cell).
+
